b3b89d6a2e155181ca50d0040ec982230b20b29f
Rajveer Sidhu <43861959+rajveersidhu@users.noreply.github.com>
2025-02-25 18:49:19 -0800

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
+from parser import parse_logs
 
+# Configuration
 SENDER_EMAIL = "your-email@example.com"
 SENDER_PASSWORD = "your-password"
 RECIPIENT_EMAIL = "admin@example.com"
@@ -13,7 +15,7 @@ def send_alert(ip_address, username, log_entry):
     - IP Address: {ip_address}
     - Log Entry: {log_entry}
     """
-    
+
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = SENDER_EMAIL
@@ -24,3 +26,9 @@ def send_alert(ip_address, username, log_entry):
         server.sendmail(SENDER_EMAIL, RECIPIENT_EMAIL, msg.as_string())
 
     print("🚀 Alert Sent Successfully!")
+
+if __name__ == "__main__":
+    logs = parse_logs()
+    if logs:
+        for entry in logs:
+            send_alert(entry[1], entry[0], entry[2])
