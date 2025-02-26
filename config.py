00ee5dc786cfc720411892ade18856a4d8fd6eec
Rajveer Sidhu <43861959+rajveersidhu@users.noreply.github.com>
2025-02-25 18:49:34 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+LOG_FILE = "/var/log/auth.log"
+EMAIL_ALERTS = True  # Set to False to disable email alerts
+
+# Email settings
+SENDER_EMAIL = "your-email@example.com"
+SENDER_PASSWORD = "your-password"
+RECIPIENT_EMAIL = "admin@example.com"
